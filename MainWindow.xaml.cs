3a9afe238069f3a65e10e217bcf3e30c3d707b6b
Maria <100736227+nandafachini@users.noreply.github.com>
2022-10-11 17:05:50 -0300

    Second Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CRUDProject
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = new MainWindowVM();
+            InitializeComponent();
+            saveButton.IsEnabled = true;
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Save_Button(object sender, RoutedEventArgs e)
+        {
+            removeButton.IsEnabled = false;
+            addButton.IsEnabled = false;
+            cadButton.IsEnabled = false;
+            updateButton.IsEnabled = false;
+            visualizeButton.IsEnabled = false;
+            nicknameButton.IsEnabled = false;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ MainWindowVM.cs
@@ -0,0 +1,137 @@
+﻿using CRUDProject.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Xml.Linq;
+
+namespace CRUDProject
+{
+    public class MainWindowVM : INotifyPropertyChanged
+    {
+
+        public ObservableCollection<Acai> listaAcais { get; set; }
+
+        public ObservableCollection<Usuario> listaUsuarios { get; set; }
+
+        public String nickname { get; set;}
+
+        public ICommand Add { get; private set; }
+
+        public ICommand Remove { get; private set; }
+
+        public Acai AcaiSelecionado { get; set; }
+
+        public ICommand AddUsuario { get; set; }
+
+        public ICommand UpdateAcai { get; set; }
+        
+        public ICommand SearchAcai { get; set; }
+
+        public ICommand ShowAcais { get; set; }
+
+        public ICommand SaveNickname { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public MainWindowVM()
+        {
+            listaAcais = new ObservableCollection<Acai>()
+            {
+                
+            };
+
+            listaUsuarios = new ObservableCollection<Usuario>();
+
+            IniciaComandos();
+        }
+
+        public void IniciaComandos()
+        {
+            Add = new RelayCommand((object param) =>
+            {
+                Acai yourAcai = new Acai();
+
+
+                CadastroAcai telaCadastro = new CadastroAcai();
+                telaCadastro.DataContext = yourAcai;
+                telaCadastro.ShowDialog();
+
+                listaAcais.Add(yourAcai);
+
+               
+            });
+
+            Remove = new RelayCommand((object param) =>
+            {
+                listaAcais.Remove(AcaiSelecionado);
+            });
+
+            AddUsuario = new RelayCommand((object param) =>
+            {
+                Usuario userCadastro = new Usuario();
+
+                CadastroUsuario telaCadastroUsuario = new CadastroUsuario();
+                telaCadastroUsuario.DataContext = userCadastro;
+                telaCadastroUsuario.ShowDialog();
+
+                listaUsuarios.Add(userCadastro);
+            });
+
+            UpdateAcai = new RelayCommand((object param) =>
+            {
+
+                if (AcaiSelecionado != null)
+                {
+                    CadastroAcai telaCadastro = new CadastroAcai();
+                    telaCadastro.DataContext = AcaiSelecionado;
+                    telaCadastro.ShowDialog();
+                } 
+
+            });
+
+            SearchAcai = new RelayCommand((object param) =>
+            {
+
+                if (AcaiSelecionado.Tamanho != null)
+                {
+                    CadastroAcai telaCadastro = new CadastroAcai();
+                    telaCadastro.DataContext = AcaiSelecionado.Tamanho;
+                    telaCadastro.ShowDialog();
+                }
+
+            });
+
+            ShowAcais = new RelayCommand((object param) =>
+            {
+                VerPedidoTotal telaPedido = new VerPedidoTotal();
+                telaPedido.DataContext = listaAcais;
+                telaPedido.ShowDialog();
+            });
+
+            SaveNickname = new RelayCommand((object param) =>
+            {
+                
+                
+            });
+
+        }
+
+    }
+}
