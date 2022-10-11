90a007331f7454ba6fd95550a788a43ff47e0193
Maria <100736227+nandafachini@users.noreply.github.com>
2022-10-11 17:06:26 -0300

    adicionando classes

---
+++ Acai.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDProject
+{
+    public class Acai
+    {
+        private string tamanho;
+
+        private string fruta;
+
+        private string complemento;
+
+        public Acai()
+        {
+
+        }
+
+        public Acai(string tamanho, string fruta, string complemento)
+        {
+            this.tamanho = tamanho;
+            this.fruta = fruta;
+            this.complemento = complemento;
+        }
+
+        public string Tamanho
+        {
+            get { return tamanho; }
+            set { tamanho = value; }
+        }
+
+        public string Fruta
+        {
+            get { return fruta; }
+            set { fruta = value; }
+        }
+
+        public string Complemento
+        {
+            get { return complemento; }
+            set { complemento = value; }
+        }
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDProject
+{
+    public class Usuario
+    {
+        private string name;
+
+        private string endereco;
+
+        private string cpf;
+        public Usuario()
+        {
+
+        }
+
+        public Usuario(string name, string endereco, string cpf)
+        {
+            this.name = name;
+            this.endereco = endereco;
+            this.cpf = cpf;
+
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Endereco
+        {
+            get { return endereco; }
+            set { endereco = value; }
+        }
+
+        public string Cpf
+        {
+            get { return cpf; }
+            set { cpf = value; }
+        }
+    }
+}
+
